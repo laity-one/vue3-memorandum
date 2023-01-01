bd8b9343831aa7d4247f6b0fe962dca3ae6f3bbc
peibin wang <wangpeibin@peibindebijibendiannao.local>
2023-01-01 19:34:26 +0800

    feat: 新增项目配置eslint，commit规范等

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+import './style/common.less'
 createApp(App).use(router).use(store).mount('#app')
